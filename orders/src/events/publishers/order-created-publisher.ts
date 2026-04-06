bf535218bd240a84d60707f47d32c7a1e36e0e1d
azizbenayed <mohamedaziz.benayed@gmail.com>
2026-04-06 15:58:45 +0100

    clean repo

--- orders/src/events/publishers/order-created-publisher.ts
+++
@@ -1,7 +0,0 @@
-import { Producer, OrderCreatedEvent, ExchangeNames } from "@eftickets/common";
-
-export class OrderCreatedPublisher extends Producer<OrderCreatedEvent> {
-  readonly exchangeName = ExchangeNames.OrderCreated;
-  routingKey = "ordersKeyCreate";
-  exchangeType = "direct";
-}
